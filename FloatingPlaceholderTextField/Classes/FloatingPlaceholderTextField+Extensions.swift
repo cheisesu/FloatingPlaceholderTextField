ecf826a86a466bb730e47772ed76242751b9799f
Dmitry Shelonin <cheisesu@gmail.com>
2021-04-06 19:42:39 +0300

    - some moves;

--- FloatingPlaceholderTextField/Classes/FloatingPlaceholderTextField+Extensions.swift
+++ FloatingPlaceholderTextField/Classes/FloatingPlaceholderTextField+Extensions.swift
@@ -1,20 +1,3 @@
 import UIKit
 
-extension FloatingPlaceholderTextField {
-    public typealias Attributes = [NSAttributedString.Key: Any]
-    
-    public struct State: OptionSet, Hashable {
-        public let rawValue: Int
-        
-        public static let `default` = State()
-        /// Состояние, когда поле в фокусе
-        public static let active = State(rawValue: 1 << 0)
-        /// Состояние, когда в поле есть контент
-        public static let filled = State(rawValue: 1 << 1)
-        public static let disabled = State(rawValue: 1 << 2)
-        
-        public init(rawValue: Int) {
-            self.rawValue = rawValue
-        }
-    }
-}
+
--- FloatingPlaceholderTextField/Classes/FloatingPlaceholderTextField.swift
+++ FloatingPlaceholderTextField/Classes/FloatingPlaceholderTextField.swift
@@ -1,5 +1,24 @@
 import UIKit
 
+extension FloatingPlaceholderTextField {
+    public typealias Attributes = [NSAttributedString.Key: Any]
+    
+    public struct State: OptionSet, Hashable {
+        public let rawValue: Int
+        
+        public static let `default` = State()
+        /// Состояние, когда поле в фокусе
+        public static let active = State(rawValue: 1 << 0)
+        /// Состояние, когда в поле есть контент
+        public static let filled = State(rawValue: 1 << 1)
+        public static let disabled = State(rawValue: 1 << 2)
+        
+        public init(rawValue: Int) {
+            self.rawValue = rawValue
+        }
+    }
+}
+
 /// Поле для ввода текста с перемещающимся наверх placeholder
 open class FloatingPlaceholderTextField: UITextField {
     // MARK: - PRIVATE PROPERTIES
